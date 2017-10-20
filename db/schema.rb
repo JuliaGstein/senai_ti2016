95db0cd06d97668d377fa0a53ac948858479a69b
Breno Thales <breno.thales@gmail.com>
2017-10-20 00:16:24 +0000

    Add user_id na tabela tasks e criando relacionameto

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171019232525) do
+ActiveRecord::Schema.define(version: 20171020000218) do
 
   create_table "tasks", force: :cascade do |t|
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
